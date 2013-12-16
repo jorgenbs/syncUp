ca47a62424da2df3b63d87d96e646cb2efd8521b
jorgenbs <jorgenbs@gmail.com>
2013-12-16 13:40:05 +0100

    tests are broken

---
+++ README.md
@@ -0,0 +1,2 @@
+Run code across tabs/browsers at the same time, by the help of socket.io.
+
--- spec/tests.spec.js
+++ spec/tests.spec.js
@@ -2,22 +2,60 @@ var syncup  = require('../lib/syncup')
   , server  = require('../lib/server');
 
 describe("loading", function () {
+
   it("module should be loaded as object", function () {
+
     expect(typeof syncup).toBe("object");
+
   });
+
   it("module have a function called sync", function() {
+
     expect(typeof syncup.sync).toBe("function")
+  
   });
 });
 
 describe("callback", function() {
-  it("should not trigger callback if specified", function () {
 
+  it("should be able to specify timeout", function () {
     runs(function () {
       this.triggered = false;
       var self = this;
-      syncup.sync('localhost:8080', false, function () {
-        self.triggered = true;
+
+      syncup.sync({
+        socketUrl: 'http://localhost:8080', 
+        trigger: true,
+        timeout: 1,
+        callback: function () {
+          self.triggered = true;
+        }
+      });
+    });
+
+    waits(1500);
+
+    runs(function() {
+      expect(this.triggered).toBe(true);
+    });
+
+  });
+});
+
+describe("callback", function() {
+
+  it("should not trigger callback if specified", function () {
+
+    runs(function () {
+
+      this.triggered = false;
+      var self = this;
+
+      syncup.sync({
+        socketUrl: 'localhost:8080', 
+        callback: function () {
+          self.triggered = true
+        }
       });
     });
 
@@ -28,22 +66,53 @@ describe("callback", function() {
     });
 
   });
+});
+
+describe("callback", function() {
 
   it("should trigger callback if specified", function () {
+
     runs(function () {
       this.triggered = false;
       var self = this;
-      syncup.sync('localhost:8080', true, function () {
-        self.triggered = true;
-        console.log('triggered!');
+
+      syncup.sync({
+        socketUrl: 'http://localhost:8080', 
+        trigger: true, 
+        callback: function () {
+          self.triggered = true;
+        }
       });
     });
 
-    waits(1000);
+    waits(6000);
 
     runs(function () {
       expect(this.triggered).toBe(true);
     });
   });
+});
 
+describe("callback", function() {
+
+  it("should default to 5 seconds if timeout not specified", function() {
+    runs(function () {
+      this.triggered = false;
+      var self = this;
+
+      syncup.sync({
+        socketUrl: 'http://localhost:8080', 
+        trigger: true, 
+        callback: function () {
+          self.triggered = true;
+        }
+      });
+    });
+
+    waits(1000);
+
+    runs(function() {
+      expect(this.triggered).toBe(false);
+    });
+  });
 });
